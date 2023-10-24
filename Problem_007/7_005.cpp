9b9b98576b23a84fe23be71ca0e9a2972b485676
Tuệ Dương Thông <23521734@gm.uit.edu.vn>
2023-10-25 00:42:45 +0700

    7_005 + 7_010. Completed

--- Problem_007/7_005.cpp
+++ Problem_007/7_005.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
-    return 0;
+	int m, n;
+	float a[100][100];
+	float s = 0;
+	cin >> m >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+	for (int i = 0; i < n; i++)
+		s += a[i][i];
+	cout << s;
+	return 0;
 }
--- Problem_007/7_010.cpp
+++ Problem_007/7_010.cpp
@@ -3,5 +3,33 @@ using namespace std;
 
 int main()
 {
-    return 0;
+	int m, n, l, k;
+	float a[100][100];
+	float b[100][100];
+	float c[100][100];
+	cin >> m >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			cin >> a[i][j];
+
+	cin >> l >> k;
+	for (int i = 0; i < l; i++)
+		for (int j = 0; j < k; j++)
+			cin >> b[i][j];
+
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < k; j++)
+		{
+			c[i][j] = 0;
+			for (int t = 0; t < n; t++)
+				c[i][j] += a[i][t] * b[t][j];
+		}
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < k; j++)
+			cout << c[i][j] << " ";
+		cout << endl;
+	}
+	return 0;
 }
