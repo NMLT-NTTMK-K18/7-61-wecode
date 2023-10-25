f3d1eade008997bc490130cd69f05bd0c0e7bed7
NTGNguyen <23521049@gm.uit.edu.vn>
2023-10-25 20:04:36 +0700

    ✅ Problem6_05.Done

--- Problem_006/6_003.cpp
+++ Problem_006/6_003.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int a[500];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int Number_InsertTheMidle;
+    cin >> Number_InsertTheMidle;
+    int Insert_Position;
+    cin >> Insert_Position;
+    for (int i = n; i > Insert_Position; i--)
+    {
+        a[i] = a[i - 1];
+    }
+    a[Insert_Position] = Number_InsertTheMidle;
+    for (int i = 0; i < n + 1; i++)
+    {
+        cout << a[i] << " ";
+    }
     return 0;
 }
