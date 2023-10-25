e244e2128425293317dc79e80b3e70f043f6e1b9
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-25 20:39:55 +0700

    💥 6_005 & 6_100 (undone)

--- Problem_006/6_005.cpp
+++ Problem_006/6_005.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
 using namespace std;
+#define MAX 100
+
+void Nhapmang(int a[], int &n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+}
+
+bool firstOne(int a)
+{
+    int temp = 0;
+    while (a)
+    {
+        temp = a % 10;
+        a /= 10;
+    }
+    if (temp == 1)
+        return true;
+    return false;
+}
+
+bool firstFour(int a)
+{
+    int temp = 0;
+    while (a)
+    {
+        temp = a % 10;
+        a /= 10;
+    }
+    if (temp == 4)
+        return true;
+    return false;
+}
+
+bool isBenford(int a[], int n)
+{
+    int count_first_1 = 0;
+    int count_first_4 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (firstOne(a[i]))
+            count_first_1++;
+        else if (firstFour(a[i]))
+            count_first_4++;
+    }
+    if (count_first_1 == 3 && count_first_4 == 1)
+        return true;
+    return false;
+}
 
 int main()
 {
+    int a[MAX], n = 10;
+    Nhapmang(a, n);
+    if (isBenford(a, n) == true)
+        cout << "TRUE" << endl;
+    else
+        cout << "FALSE" << endl;
     return 0;
 }
--- Problem_006/6_010.cpp
+++ Problem_006/6_010.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main()
 {
+    // undone
+    float a[100], b[100], c[200];
+    int array_a, array_b;
+    cin >> array_a;
+    for (int i = 0; i < array_a; i++)
+        cin >> a[i];
+    cin >> array_b;
+    for (int i = 0; i < array_b; i++)
+        cin >> b[i];
+    int m = 0, n = 0;
+    int array_c = array_a + array_b;
+    for (int i = 0; i < array_c; i++)
+        if (a[m] < b[n] || n >= array_b)
+            c[i] = a[m++];
+        else
+            c[i] = b[n++];
+    for (int i = 0; i < array_c; i++)
+        cout << c[i] << " ";
     return 0;
 }
