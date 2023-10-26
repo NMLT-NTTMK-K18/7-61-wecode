8355c86246485a09f5ee23a083bfb33ec1192ef1
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-10-26 22:52:54 +0700

    6_004 completed

--- Problem_006/6_004.cpp
+++ Problem_006/6_004.cpp
@@ -1,61 +1,69 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 #define MAX 100
 
-void NhapmangSNT(int a[], int &n);
-int SoPhanTuChuaY(int a[], int n,int y);
+void NhapmangSNT(int a[], int& n);
+int SoPhanTuChuaY(int a[], int n, int y);
 
 int main()
 {
-	int a[MAX], n, y;
-	cin >>y;
-	NhapmangSNT(a,n);
-	cout << SoPhanTuChuaY(a, n, y) << endl;;
-	return 0;
+    int a[MAX], n, y;
+    cin >> y;
+    NhapmangSNT(a, n);
+    for (int i = 0; i < n; i++)
+        cout << a[i] << setw(5);
+    cout << SoPhanTuChuaY(a, n, y)<<endl;
+    return 0;
 }
 
-void NhapmangSNT(int a[],int& n)
+void NhapmangSNT(int a[], int& n)
 {
-    cin>>n;
-    for(int i=0;i<n;i++)
+    cin >> n;
+    int chuyenvong;
+    if(n>0)
+    a[0]=2;
+    else
+    cout<<0;
+    for (int i = 1; i < n; i++)
     {
-        int demnt=0;
-        int dem=0;
-        for(int j=2;dem<n;j++)
+        chuyenvong = 0;
+        for (int j = 2; chuyenvong == 0; j++)
         {
-           for(int k=1;k<=j;k++)
-           {
-                if(j%k==0)
-                   demnt++;
-           }
-           if(demnt==2)
-           {  
-             a[i]=j;
-             dem++;
-           }
+            int dem = 0;
+            for (int k = 1; k <= j; k++)
+            {
+                if (j % k == 0)
+                    dem++;
+            }
+            if (dem == 2 && j > a[i - 1])
+            {
+                a[i] = j;
+                chuyenvong++;
+            }
         }
-             
-
     }
+
 }
 
-int SoPhanTuChuaY(int a[], int n,int y)
+int SoPhanTuChuaY(int a[], int n, int y)
 {
-    int dem=0;
-    for(int i=0;i<n;i++)
+    int dem = 0;
+    for (int i = 0; i < n; i++)
     {
-        int flag=0;
-        int t=a[i];
-        while(t>0)
+        int t = a[i];
+        int flag = 0;
+        while (t > 0)
         {
-            int du= t %10;
-            if(du==y)
-               flag=1;
-            t=t/10;
+            int du = t % 10;
+            if (du == y)
+                flag = 1;
+            t /= 10;
         }
-        if(flag ==1)
+        if (flag == 1)
             dem++;
     }
     return dem;
+
 }
