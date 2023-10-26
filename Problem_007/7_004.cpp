6390a717564492c5162aafbad56bd8e7833f68e1
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-26 08:46:38 +0700

    7.004 Done

--- Problem_007/7_004.cpp
+++ Problem_007/7_004.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+    float a[100][100];
+    int m, n;
+    cin >> m >> n;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cout << a[j][i] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
