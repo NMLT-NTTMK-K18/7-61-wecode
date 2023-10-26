6bcca29fe73910c038908930dbe0849bdcd6aeb4
khiemhoangtruongUIT <137704231+truonghoangkhiem@users.noreply.github.com>
2023-10-26 21:24:14 +0700

    Lesson 7 has some errors

--- Problem_006/6_004.cpp
+++ Problem_006/6_004.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+#define MAX 100
+
+void NhapmangSNT(int a[], int &n);
+int SoPhanTuChuaY(int a[], int n,int y);
 
 int main()
 {
-    return 0;
+	int a[MAX], n, y;
+	cin >>y;
+	NhapmangSNT(a,n);
+	cout << SoPhanTuChuaY(a, n, y) << endl;;
+	return 0;
+}
+
+void NhapmangSNT(int a[],int& n)
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int demnt=0;
+        int dem=0;
+        for(int j=2;dem<n;j++)
+        {
+           for(int k=1;k<=j;k++)
+           {
+                if(j%k==0)
+                   demnt++;
+           }
+           if(demnt==2)
+           {  
+             a[i]=j;
+             dem++;
+           }
+        }
+             
+
+    }
+}
+
+int SoPhanTuChuaY(int a[], int n,int y)
+{
+    int dem=0;
+    for(int i=0;i<n;i++)
+    {
+        int flag=0;
+        int t=a[i];
+        while(t>0)
+        {
+            int du= t %10;
+            if(du==y)
+               flag=1;
+            t=t/10;
+        }
+        if(flag ==1)
+            dem++;
+    }
+    return dem;
 }
--- Problem_006/6_007.cpp
+++ Problem_006/6_007.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+
 using namespace std;
+
+void Nhap(int[], int&);
+int GTLN(int[],int);
 
 int main()
 {
-    return 0;
+	int a[5000];
+	int n;
+	Nhap(a, n);
+	cout << GTLN(a, n);
+	return 0;
 }
+
+void Nhap(int a[], int& n)
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+int GTLN(int a[],int n)
+{
+	int max2 = a[0];
+	int max = a[0];
+	for (int i = 0; i < n; i++)
+		if (max < a[i])
+			max = a[i];
+	for (int i = n-1; i>=0; i--)
+		if (max > a[i] && max2 <= a[i])
+			max2 = a[i];
+	if (max == max2)
+		return 0;
+	return max2;
+}
+
