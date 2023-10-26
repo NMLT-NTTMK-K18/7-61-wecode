52f0c914df8ebeccc61e6fadcb7f95c547df7217
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-10-26 12:00:19 +0700

    💩 Swap 6_002 & 6_004

--- Problem_006/6_004.cpp
+++ Problem_006/6_004.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
 using namespace std;
-int nt(int);
 
 int main()
 {
-    int a[100];
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-
-    int flag = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (nt(a[i]) == 1)
-        {
-            flag = 1;
-            cout << a[i] << " ";
-        }
-    }
-
-    if (flag == 0)
-        cout << 0;
-    return 0;
-}
-
-int nt(int n)
-{
-    int dem = 0;
-    for (int i = 1; i <= n; i++)
-        if (n % i == 0)
-            dem++;
-    if (dem == 2)
-        return 1;
     return 0;
 }
